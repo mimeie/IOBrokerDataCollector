a6fa4c312581215503d9a384fa977d25acc8d386
Michael Meienberg <mei@acs-ag.ch>
2020-07-28 16:01:46 +0200

    neues projekt hinzugefügt

---
+++ IOBrokerDataCollectorBusinessLogic/AccessData.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IOBrokerDataCollectorBusinessLogic
+{
+    public static class AccessData
+    {
+        public static string IOBrokerApi = "http://jportal1:8087/get/zwave2.0.Node_003.Multilevel_Sensor.humidity";
+    }
+}
---
+++ IOBrokerDataCollectorBusinessLogic/Collector.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Net;
+
+using Newtonsoft.Json;
+
+namespace IOBrokerDataCollectorBusinessLogic
+{
+    public class Collector
+    {
+        public int getIntValue(string id)
+        {
+            try
+            {
+                
+                using (WebClient wc = new WebClient())
+                {
+                  
+
+                    string downString = AccessData.IOBrokerApi + id;
+                    Console.WriteLine("Download String '{0}'", downString);
+
+                    var json = wc.DownloadString(downString);                    
+
+                }
+
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("Fehler beim lesen der Leistung", ex);
+                throw;
+            }
+
+            return 0;
+        }
+    }
+}
